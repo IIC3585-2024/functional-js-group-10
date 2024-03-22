4bc251e4abe443e789d69604a4c915aeafa51e21
Cristóbal Berríos <crisberrios@uc.cl>
2024-03-22 19:32:33 -0300

    function to process emphasis in lines

--- markdownToHtml/script.js
+++ markdownToHtml/script.js
@@ -69,7 +69,7 @@ const heading = {
       return;
     }
 
-    return line === _.repeat("=", line.length) || line === _.repeat("-", line.length); // If its a line of only '=' create h1, if its only '-' create h2
+    return line === _.repeat("=", line.length) || line === _.repeat("-", line.length);
   },
   create(htmlArray, line) {
     if (this.defaultCondition(line)) this.createDefault(htmlArray, line);
@@ -77,25 +77,17 @@ const heading = {
   },
   createDefault(htmlArray, line) {
     const level = _.takeWhile(line, (char) => char === "#").length;
-    const content = _.trimStart(line, "# ");
+    let content = _.trimStart(line, "# ");
+    content = applyEmphasis(content);
+
     htmlArray.push({ type: "heading", tag: "h" + level, content });
   },
   createUnderline(htmlArray, line) {
     const lastElement = _.last(htmlArray);
     const content = lastElement.content;
-    const level = line[0] === "=" ? 1 : 2; // If its a line of only '=' create h1, if its only '-' create h2
+    const level = line[0] === "=" ? 1 : 2;
     htmlArray.pop();
     htmlArray.push({ type: "heading", tag: "h" + level, content });
-  },
-};
-
-const list = {
-  condition(line) {
-    return line.startsWith("* ");
-  },
-  create(htmlArray, line) {
-    const content = _.trimStart(line, "* ");
-    htmlArray.push({ type: "list", tag: "li", content });
   },
 };
 
@@ -107,12 +99,70 @@ const paragraph = {
       line = line + "<br>";
     }
 
+    line = applyEmphasis(line);
+
     const lastElement = _.last(htmlArray);
     if (lastElement && lastElement.type === "paragraph") {
       lastElement.content = lastElement.content + " " + line;
     } else {
       htmlArray.push({ type: "paragraph", tag: "p", content: line });
     }
+  },
+};
+
+function applyEmphasis(line) {
+  let length = 3;
+  let relativeStart = 0;
+  while (length > 0) {
+    console.log("emphasis", line);
+
+    const index1 = line.indexOf(_.repeat("*", length), relativeStart);
+    const index2 = line.indexOf(_.repeat("*", length), index1 + length);
+    if (index1 === -1 || index2 === -1) {
+      length--;
+      relativeStart = 0;
+      continue;
+    }
+
+    if (index2 - index1 === 1) {
+      relativeStart = index2;
+      continue;
+    }
+
+    line =
+      line.slice(0, relativeStart) +
+      line.slice(relativeStart, index1) +
+      emphasis[length].start +
+      line.slice(index1 + length, index2) +
+      emphasis[length].end +
+      line.slice(index2 + length);
+  }
+
+  return line;
+}
+
+const emphasis = {
+  1: {
+    start: "<em>",
+    end: "</em>",
+  },
+  2: {
+    start: "<strong>",
+    end: "</strong>",
+  },
+  3: {
+    start: "<em><strong>",
+    end: "</strong></em>",
+  },
+};
+
+const list = {
+  condition(line) {
+    return line.startsWith("* ");
+  },
+  create(htmlArray, line) {
+    const content = _.trimStart(line, "* ");
+    htmlArray.push({ type: "list", tag: "li", content });
   },
 };
 
